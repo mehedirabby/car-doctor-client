04104192537299eee198adc213d7b55740dff2d3
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-11-02 12:08:54 +0600

    first commit

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
     </>
   );
   return (
-    <div data-theme="bumblebee" className="navbar bg-base-100 h-20 mb-12">
+    <div data-theme="bumblebee" className="navbar bg-base-100 pt-12 h-20 mb-12">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
