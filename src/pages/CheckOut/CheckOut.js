0d05cc2e2afde47639460b52ed0c1d8c17cb91ea
Mehedi rabby <mehedi15-2995@diu.edu.bd>
2022-11-06 00:12:20 +0600

    loop

--- src/pages/CheckOut/CheckOut.js
+++ src/pages/CheckOut/CheckOut.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Context/UserContext/UserContext";
 
 const CheckOut = () => {
   const service = useLoaderData();
-  const { title } = service;
+  const { user } = useContext(AuthContext);
+  const { title, price, _id } = service;
+  const handlePlaceOrder = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const email = user?.email || "unregister";
+    const message = form.message.value;
+    const phone = form.phone.value;
+
+    const order = {
+      service: _id,
+      serviceName: title,
+      price,
+      customerName: name,
+      email,
+      phone,
+      message,
+    };
+  };
   return (
     <div>
-      <h2>{title}</h2>
+      <form onSubmit={handlePlaceOrder}>
+        <h2 className="text-4xl">You are about to buy:{title}</h2>
+        <h2 className="text-3xl">Price:${price}</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <input
+            name="firstName"
+            type="text"
+            placeholder="First Name"
+            className="input input-bordered w-full"
+          />
+          <input
+            name="lastName"
+            type="text"
+            placeholder="Last Name"
+            className="input input-bordered w-full input-ghost"
+          />
+          <input
+            name="phone"
+            type="text"
+            placeholder="Your Phone"
+            className="input input-bordered w-full"
+          />
+          <input
+            name="email"
+            type="text"
+            placeholder="Your Email"
+            defaultValue={user?.email}
+            className="input input-bordered w-full"
+            readOnly
+          />
+        </div>
+        <textarea
+          name="message"
+          className="textarea textarea-bordered h-24 w-full"
+          placeholder="Your Massage"
+        ></textarea>
+        <input className="btn" type="submit" value="Place Your Order" />
+      </form>
     </div>
   );
 };
